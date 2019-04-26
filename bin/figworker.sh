c0ba7385bbc1138edfa866ba73e11b5c389f0513
Esmé Cowles <escowles@ticklefish.org>
2019-04-26 11:57:01 -0400

    Adding super_low queue to worker

--- bin/figworker.sh
+++ bin/figworker.sh
@@ -3,4 +3,4 @@
 # run a figgy sidekiq worker
 
 cd $HOME/src/figgy
-bundle exec sidekiq -c 1 -q default -q low # --daemon --logfile tmp/sidekiq.log &
+bundle exec sidekiq -c 1 -q default -q low -q super_low # --daemon --logfile tmp/sidekiq.log &
