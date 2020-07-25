457a41023b17f5f5d1aac2d8145b23bbd3b49214
Esmé Cowles <escowles@ticklefish.org>
2020-07-25 07:17:24 -0400

    Allowing since.sh to handle future dates gracefully

--- bin/since.sh
+++ bin/since.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-PAST=$1
-if [ ! "$PAST" ]; then
-  PAST=2020-03-12
+OTHER=$1
+if [ ! "$OTHER" ]; then
+  OTHER=2020-03-12
 fi
 
 NOW=`date +%s`
-THEN=`date -j -f "%Y-%m-%d" +%s "$PAST"`
-echo $(( ($NOW - $THEN) / (3600*24) )) days since $PAST
+THEN=`date -j -f "%Y-%m-%d" +%s "$OTHER"`
+DIFF=$(( ($NOW - $THEN) / (3600*24) ))
+if [ ${DIFF:0:1} = "-" ]; then
+  echo ${DIFF:1} days until $OTHER
+else
+  echo $DIFF days since $OTHER
+fi
