00c160145a152e789134c84a4d330ca087531ee2
Esmé Cowles <escowles@ticklefish.org>
2020-08-18 16:16:58 -0400

    Updating imapsync.rb to support multiple sync profiles

--- bin/imapsync.rb
+++ bin/imapsync.rb
@@ -4,22 +4,6 @@
 #
 # configuration goes in ~/.imapsync/config.yml, which should look like:
 #
-# source:
-#   host: mail.example.com
-#   user: user@example.com
-#   pass: XXXXXXXXXXXX
-#   mbox: From Mailbox
-# destination:
-#   host: mail.example.org
-#   user: user
-#   pass: XXXXXXXXXXXX
-#   mbox: Archive.<%= Date.today.year %>.<%= Date.today.strftime("%m") %>
-#
-# messages from the source server/mbox will be copied to the destination server/mbox
-# (without duplication). All values can contain ERB interpolation, including Dates.
-#
-# TODO: could reorg this to have as many source/destination pairs as desired, which
-# could look like:
 # servers:
 #   server1:
 #      host: mail.example.com
@@ -43,6 +27,10 @@
 #      from_mbox: Sent Messages
 #      to_server: server3
 #      to_mbox: sent
+#
+# for each sync entry, messages from the "from" server/mbox will be copied to the "to"
+# server/mbox (without duplication). All values can contain ERB interpolation,
+# including Dates.
 
 # requirements: net/imap, plus yaml/erb/date for config
 require "date"
@@ -50,57 +38,62 @@ require "erb"
 require "net/imap"
 require "yaml"
 
+@connection_pool = {}
+
 # check if a message with the given msg_id exists in the given imap server
 def exists?(imap, msg_id)
   imap.search(["HEADER", "Message-ID", msg_id]).any?
 end
 
-# load configuration from yaml file
-config = YAML.load(ERB.new(File.read("#{ENV["HOME"]}/.imapsync/config.yml")).result)
-src_email = config["source"]["user"]
-src_host = config["source"]["host"]
-src_pass = config["source"]["pass"]
-src_mbox = config["source"]["mbox"]
-dst_email = config["destination"]["user"]
-dst_host = config["destination"]["host"]
-dst_pass = config["destination"]["pass"]
-dst_mbox = config["destination"]["mbox"]
+# load a connection from the pool, or connect to the server
+def get_connection(profile)
+  return @connection_pool[profile] if @connection_pool[profile]
 
-# connect to source imap and login
-src_imap = Net::IMAP.new(src_host, 993, true)
-src_imap.login(src_email, src_pass)
-
-# connect to destination
-dst_imap = Net::IMAP.new(dst_host, 993, true)
-dst_imap.login(dst_email, dst_pass)
-
-# open or create destination mailbox
-unless dst_imap.list("", dst_mbox)
-  puts "creating destination: #{dst_mbox}"
-  dst_imap.create(dst_mbox)
+  props = @config["servers"][profile]
+  con = Net::IMAP.new(props["host"], 993, true)
+  con.login(props["user"], props["pass"])
+  @connection_pool[profile] = con
+  con
 end
-dst_imap.select(dst_mbox)
 
-# select source mbox, list messages, and copy messages that don't already exist to
-# destination mbox
-skip = 0
-copy = 0
-stat = src_imap.examine(src_mbox)
-src_imap.search(["TO", "*"]).each do |message_id|
-  msg = src_imap.fetch(message_id, ["ENVELOPE", "RFC822", "FLAGS", "INTERNALDATE"]).first
-  msg_id = msg.attr['ENVELOPE'].message_id
-  if exists?(dst_imap, msg_id)
-    skip += 1
-    puts "#{copy}/#{skip} skip #{msg_id}"
-  else
-    copy += 1
-    puts "#{copy}/#{skip} copy #{msg_id}"
-    dst_imap.append(dst_mbox, msg.attr['RFC822'], msg.attr['FLAGS'], msg.attr['INTERNALDATE'])
+# load configuration from yaml file
+@config = YAML.load(ERB.new(File.read("#{ENV["HOME"]}/.imapsync/config.yml")).result)
+
+# iterate over each sync profile and copy messages
+@config["sync"].each do |name, props|
+  # connect to servers
+  src_imap = get_connection(props["from_server"])
+  dst_imap = get_connection(props["to_server"])
+
+  # open or create destination mailbox
+  dst_mbox = props["to_mbox"]
+  unless dst_imap.list("", dst_mbox)
+    puts "creating destination: #{dst_mbox}"
+    dst_imap.create(dst_mbox)
+  end
+  dst_imap.select(dst_mbox)
+
+  # select source mbox, list messages, and copy messages that don't already exist to
+  # destination mbox
+  skip = 0
+  copy = 0
+  stat = src_imap.examine(props["from_mbox"])
+  src_imap.search(["TO", "*"]).each do |message_id|
+    msg = src_imap.fetch(message_id, ["ENVELOPE", "RFC822", "FLAGS", "INTERNALDATE"]).first
+    msg_id = msg.attr['ENVELOPE'].message_id
+    if exists?(dst_imap, msg_id)
+      skip += 1
+      puts "#{name} #{copy}/#{skip} skip #{msg_id}"
+    else
+      copy += 1
+      puts "#{name} #{copy}/#{skip} copy #{msg_id}"
+      dst_imap.append(dst_mbox, msg.attr['RFC822'], msg.attr['FLAGS'], msg.attr['INTERNALDATE'])
+    end
   end
 end
 
-# logout
-src_imap.logout
-src_imap.disconnect
-dst_imap.logout
-dst_imap.disconnect
+# logout of all connected servers
+@connection_pool.each do |_name, con|
+  con.logout
+  con.disconnect
+end
