f30566f55f8cc7623eea5d2c4b236133885df4ac
Esmé Cowles <escowles@ticklefish.org>
2020-09-27 16:27:53 -0400

    New office light layout

--- bin/office.sh
+++ bin/office.sh
@@ -2,18 +2,15 @@
 
 # use hue to set office light levels
 
-FULL_LIGHTS=3,15
-HALF_LIGHTS=6,16
+LIGHTS=7,8,9
 VAL=210
 if [ "$1" ]; then
   VAL=$1
 fi
-HALF_VAL=$(( $VAL / 2 ))
 
 if [ "$1" = 'off' -o "$1" = '0' ]; then
-  hue lights $HALF_LIGHTS,$FULL_LIGHTS off
+  hue lights $LIGHTS off
 else
-  hue lights $HALF_LIGHTS,$FULL_LIGHTS on
-  hue lights $FULL_LIGHTS =$VAL
-  hue lights $HALF_LIGHTS =$HALF_VAL
+  hue lights $LIGHTS on
+  hue lights $LIGHTS =$VAL
 fi
