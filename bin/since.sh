c24e7edb387b129b8dd5ebe6c9808d3c5a79f89e
Esmé Cowles <escowles@ticklefish.org>
2021-01-20 08:00:21 -0500

    adding support for today being the target date

--- bin/since.sh
+++ bin/since.sh
@@ -10,6 +10,8 @@ THEN=`date -j -f "%Y-%m-%d" +%s "$OTHER"`
 DIFF=$(( ($NOW - $THEN) / (3600*24) ))
 if [ ${DIFF:0:1} = "-" ]; then
   echo ${DIFF:1} days until $OTHER
+elif [ "$DIFF" = "0" ]; then
+  echo today is $OTHER
 else
   echo $DIFF days since $OTHER
 fi
