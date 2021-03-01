bbc32d1b99406111e950bb4348da250cdffe7138
Esmé Cowles <escowles@ticklefish.org>
2021-03-01 12:46:41 -0500

    adjusting light scripts

--- bin/office.sh
+++ bin/office.sh
@@ -2,15 +2,19 @@
 
 # use hue to set office light levels
 
-LIGHTS=7,8,9
+FULL_LIGHTS=7,8,9
+HALF_LIGHTS=1
+ALL_LIGHTS=$FULL_LIGHTS,$HALF_LIGHTS
 VAL=210
 if [ "$1" ]; then
   VAL=$1
 fi
+HALF_VAL=$(( $VAL / 2 ))
 
 if [ "$1" = 'off' -o "$1" = '0' ]; then
-  hue lights $LIGHTS off
+  hue lights $ALL_LIGHTS off
 else
-  hue lights $LIGHTS on
-  hue lights $LIGHTS =$VAL
+  hue lights $ALL_LIGHTS on
+  hue lights $FULL_LIGHTS =$VAL
+  hue lights $HALF_LIGHTS =$HALF_VAL
 fi
