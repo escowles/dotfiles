373fc588fdae4feb8d3f42003274aff0633de244
Esmé Cowles <escowles@ticklefish.org>
2025-04-18 06:56:49 -0400

    adding extra param to since.sh

--- bin/since.sh
+++ bin/since.sh
@@ -9,9 +9,9 @@ NOW=`date +%s`
 THEN=`date -j -f "%Y-%m-%d" +%s "$OTHER"`
 DIFF=$(( ($NOW - $THEN) / (3600*24) ))
 if [ ${DIFF:0:1} = "-" ]; then
-  echo ${DIFF:1} days until $OTHER
+  echo ${DIFF:1} days until $OTHER $2
 elif [ "$DIFF" = "0" ]; then
-  echo today is $OTHER
+  echo today is $OTHER $2
 else
-  echo $DIFF days since $OTHER
+  echo $DIFF days since $OTHER $2
 fi
