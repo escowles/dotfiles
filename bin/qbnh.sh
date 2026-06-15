cf91aed2205ed1eee9245bc775bccf862a367784
Esmé Cowles <escowles@ticklefish.org>
2026-06-15 14:03:08 -0400

    outputting percentage

--- bin/qbnh.sh
+++ bin/qbnh.sh
@@ -4,3 +4,11 @@ for i in ~/desktop/Screenshot*.png; do
   FN=`basename "$i" .png | sed -e's/Screenshot //' -e's/ at /T0/' -e's/.AM//' -e's/\./-/g'`
   mv -v "$i" ~/downloads/qbnh/qbnh-${FN}.png
 done
+
+START_DATE=2026-03-29
+START_S=`date -j -f "%Y-%m-%d" +%s $START_DATE`
+TODAY_S=`date +%s`
+DIFF=$(( ($TODAY_S - $START_S) / (3600*24) ))
+QBNH=`ls ~/downloads/qbnh | wc -l | tr -d ' '`
+PCT=$(( ($QBNH * 100) / $DIFF ))
+echo "$QBNH / $DIFF ($PCT%)"
